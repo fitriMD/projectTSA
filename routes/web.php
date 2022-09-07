0470da235d0ccf012c2e7c07167259d7d50440a1
SriKynanti <kynanti.ti@gmail.com>
2022-09-07 21:18:38 +0700

    Controller-Route

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,8 @@ Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/about', [AboutController::class, 'index']);
 
-Route::get('blog', [BlogController::class, 'index']);
+Route::get('/blog', [BlogController::class, 'index']);
 
-Route::get('contact', [ContactController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
 
-Route::get('product', [ProductController::class, 'index']);
+Route::get('/product', [ProductController::class, 'index']);
