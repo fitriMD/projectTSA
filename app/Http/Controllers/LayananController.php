a60c6e2de2d6e7f0d5aacfd45d4dd56ca0907a37
SriKynanti <kynanti.ti@gmail.com>
2022-09-07 21:21:52 +0700

    Controller-Route

--- app/Http/Controllers/LayananController.php
+++ app/Http/Controllers/LayananController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BlogController extends Controller
+class LayananController extends Controller
 {
     public function index() {
-        return view('blog');
+        return view('layanan');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\AboutController;
-use App\Http\Controllers\BlogController;
+use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\LayananController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,8 +28,8 @@ Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/about', [AboutController::class, 'index']);
 
-Route::get('/blog', [BlogController::class, 'index']);
+Route::get('/berita', [BeritaController::class, 'index']);
 
 Route::get('/contact', [ContactController::class, 'index']);
 
-Route::get('/product', [ProductController::class, 'index']);
+Route::get('/layanan', [LayananController::class, 'index']);
