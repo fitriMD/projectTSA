cab988aedd3f05a9257aef1613f7958a5074fd03
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-09-12 15:22:25 +0700

    insert layanan

---
+++ database/seeders/LayananSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LayananSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('layanan')->insert([
+            [
+            'judul' => 'Madiun Lor adalah sebuah kelurahan di wilayah Kecamatan Manguharjo, Kota Madiun, Provinsi Jawa Timur. 
+                            Di kelurahan inilah berada hampir semua kantor instansi pemerintah. Inka sebagai kebanggaan Indonesia yang memproduksi kereta 
+                            api baik untuk kebutuhan domestik maupun ekspor ada di wilayah ini...',
+            'isi' => '',
+            'foto' => 'img/inka.jpg',
+            'kategori_id' => ''
+            ]
+        ]);
+    }
+}
