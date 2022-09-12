ef40c6572b4aac5b4cb6faa7f501e5e0a2da9552
fitriMD <mutiara.devi027@gmail.com>
2022-09-12 22:40:05 +0700

    integrasi content

--- app/Http/Controllers/LayananController.php
+++ app/Http/Controllers/LayananController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Layanan;
+
 
 class LayananController extends Controller
 {
     public function index() {
+        $layanan = Layanan::with('kategori')->get();
         return view('layanan');
+
     }
 }
