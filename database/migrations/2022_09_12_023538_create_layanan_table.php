30e56b9e2a65323d905d24f1ce24ac6514426e05
ardilalukita14 <lukitasari623@gmail.com>
2022-09-12 10:42:21 +0700

    migration

---
+++ database/migrations/2022_09_12_023538_create_layanan_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLayananTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('layanan', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('isi');
+            $table->string('foto');
+            $table->bigInteger('kategori_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('layanan');
+    }
+}
